076466113db33d2bf0056d8b31f526d86635757f
Abigail Watson <abigailwatson@Abigails-Mac-mini-379644.local>
2018-09-09 00:16:12 -0500

    simpl-schema

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-patient',
-  version: '3.4.3',
+  version: '3.4.4',
   summary: 'HL7 FHIR Resource - Patient',
   git: 'https://github.com/clinical-meteor/hl7-resource-patient',
   documentation: 'README.md'
