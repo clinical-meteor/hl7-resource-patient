a83e3c475e50987003d0374ab2e1352c4cb98da5
Abigail Watson <awatson1978@gmail.com>
2019-08-29 15:57:23 -0500

    CardiacAccreditation MVP1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-patient',
-  version: '4.0.12',
+  version: '4.1.0',
   summary: 'HL7 FHIR Resource - Patient',
   git: 'https://github.com/clinical-meteor/hl7-resource-patient',
   documentation: 'README.md'
