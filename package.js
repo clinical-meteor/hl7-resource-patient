c1ca6cc6d7cf92e22dba26d6ae449a7c1f1677e0
Abigail Watson <awatson1978@gmail.com>
2020-02-06 18:44:38 -0600

    re-enabling npm dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-patient',
-  version: '5.2.10',
+  version: '5.2.11',
   summary: 'HL7 FHIR Resource - Patient',
   git: 'https://github.com/clinical-meteor/hl7-resource-patient',
   documentation: 'README.md'
@@ -10,51 +10,50 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
-  // api.use('meteor-base@1.4.0');
-  // api.use('ecmascript@0.13.0');
+  api.use('meteor-base@1.4.0');
+  api.use('ecmascript@0.13.0');
 
-  // api.use('mongo');
-  // api.use('session');
-  // api.use('http');
+  api.use('mongo');
+  api.use('session');
+  api.use('http');
 
-  // api.use('aldeed:collection2@3.0.0');
-  // api.use('clinical:hl7-resource-datatypes@4.0.5');
-  // api.use('clinical:hl7-resource-bundle@1.6.3');
+  api.use('aldeed:collection2@3.0.0');
+  api.use('clinical:hl7-resource-datatypes@4.0.5');
+  api.use('clinical:hl7-resource-bundle@1.6.3');
 
-  // api.use('simple:json-routes@2.1.0');
-  // api.use('react-meteor-data@0.2.15');
-  // api.use('clinical:extended-api@2.5.0');
-  // api.use('matb33:collection-hooks@0.7.15');  
+  api.use('simple:json-routes@2.1.0');
+  api.use('react-meteor-data@0.2.15');
+  api.use('clinical:extended-api@2.5.0');
+  api.use('matb33:collection-hooks@0.7.15');  
 
-  // api.use('clinical:base-model@1.5.0');
-  // api.use('clinical:user-model@1.7.0');
-  // api.imply('clinical:user-model');
+  api.use('clinical:base-model@1.5.0');
+  api.use('clinical:user-model@1.7.0');
+  api.imply('clinical:user-model');
 
-  // api.addFiles('lib/Patients.js');
+  api.addFiles('lib/Patients.js');
 
-  // // api.addFiles('server/methods.js', 'server');
-  // // api.addFiles('server/rest.js', 'server');
+  // api.addFiles('server/methods.js', 'server');
+  // api.addFiles('server/rest.js', 'server');
 
-  // // if(Package['clinical:fhir-vault-server']){
-  // //   api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
-  // // }
+  // if(Package['clinical:fhir-vault-server']){
+  //   api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
+  // }
   
-  // api.export('Patient');
-  // api.export('Patients');
-  // api.export('PatientSchema');
+  api.export('Patient');
+  api.export('Patients');
+  api.export('PatientSchema');
 
-  // api.addFiles('assets/noAvatar.png', "client", {isAsset: true});    
+  api.addFiles('assets/noAvatar.png', "client", {isAsset: true});    
 
-  // api.mainModule('index.jsx', 'client');
+  api.mainModule('index.jsx', 'client');
 });
 
 
 Npm.depends({
   "simpl-schema": "1.5.3",
   "moment": "2.22.2",
-  // "validator": "10.9.0",
-  // "lodash": "4.17.13",
-  // "material-fhir-ui": "0.9.19",
-
+  "validator": "10.9.0",
+  "lodash": "4.17.13",
+  "material-fhir-ui": "0.9.19",
 });
 
